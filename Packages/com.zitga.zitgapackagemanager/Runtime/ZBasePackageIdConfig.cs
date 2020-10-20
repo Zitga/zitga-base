0e5070465dc1083a7abb8a90f3fc0b9b5cbca3ab
minhdt <minhdt@zitga.com.vn>
2020-10-20 17:03:29 +0700

    + handle install package with depedencies

--- Packages/com.zitga.zitgapackagemanager/Runtime/ZBasePackageIdConfig.cs
+++ Packages/com.zitga.zitgapackagemanager/Runtime/ZBasePackageIdConfig.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class ZBasePackageIdConfig
 {
-    public static readonly string REPO = "zitga-base";
-    public static readonly string namePackageManager = "com.zitga.zitgapackagemanager";
-    public static readonly Dictionary<string, string> listPackages = new Dictionary<string, string>() {
+    public static readonly string Repo = "zitga-base";
+    public static readonly string NamePackageManager = "com.zitga.zitgapackagemanager";
+    public static readonly Dictionary<string, string> ListPackages = new Dictionary<string, string>() {
         { "com.zitga.zitgapackagemanager", "Zitga Package Manager" },
     };
 }
